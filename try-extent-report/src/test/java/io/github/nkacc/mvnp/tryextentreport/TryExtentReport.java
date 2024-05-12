57e0d40e76e81521ef703dba4e05446ed07c5a1b
Naveen S R <naveen.s.r@accenture.com>
2024-05-12 17:19:26 +0530

    added table logic in html-config.xml js

--- try-extent-report/src/test/java/io/github/nkacc/mvnp/tryextentreport/TryExtentReport.java
+++ try-extent-report/src/test/java/io/github/nkacc/mvnp/tryextentreport/TryExtentReport.java
@@ -1,28 +1,67 @@
 package io.github.nkacc.mvnp.tryextentreport;
 
-import org.testng.annotations.Test;
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
 
 public class TryExtentReport {
 
-    @Test
-    public void extendTest() throws IOException {
-        ExtentReports extend = new ExtentReports();
-        ExtentSparkReporter ehtml = new ExtentSparkReporter("target/extent.html");
+    private ExtentReports extent;
 
+    @BeforeTest
+    public void setUp() throws IOException {
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("target/extent.html");
         final File CONFIG_FILE = new File("src/test/resources/config/extentreports/html-config.xml");
-        ehtml.loadXMLConfig(CONFIG_FILE);
-        extend.attachReporter(ehtml);
+        htmlReporter.loadXMLConfig(CONFIG_FILE);
 
-        ExtentTest test = extend.createTest("MyFirstTest");
-        test.pass("Details");
-
-        extend.flush();
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
     }
 
+    @Test
+    public void test1() throws IOException {
+        ExtentTest test = extent.createTest("Test1", "This is Test1");
+        test.assignCategory("Category1");
+
+        // Log test steps
+        test.log(Status.INFO, "This step shows usage of log(status, details)");
+        test.pass("This test passed");
+    }
+
+    @Test
+    public void extendTest() throws IOException {
+        ExtentTest test = extent.createTest("MyFirstTest", "Sample description");
+        test.assignCategory("Category1", "Category2");
+
+        // Log test steps
+        test.log(Status.INFO, "This step shows usage of log(status, details)");
+        test.info("This step shows usage of info(details)");
+        test.fail("details", MediaEntityBuilder.createScreenCaptureFromPath(
+                "C:\\Users\\naveen.s.r\\OneDrive - Accenture\\Pictures\\Screenshots\\Screenshot (300).png"//"screenshot.png"
+        ).build());
+        test.addScreenCaptureFromPath("screenshot.png");
+    }
+
+    @Test
+    public void test3() throws IOException {
+        ExtentTest test = extent.createTest("Test3", "This is Test3");
+        test.assignCategory("Category3");
+
+        // Log test steps
+        test.log(Status.INFO, "This step shows usage of log(status, details)");
+        test.skip("This test was skipped");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        extent.flush();
+    }
 }
